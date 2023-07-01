d7d739e6110c71e08fa7f45b27da6b27ccf7f129
Omsai <omsainagvekar100@gmail.com>
2023-07-01 16:34:30 +0530

    Login Page Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import './index.css'
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      <h1>Build the disney clone boy</h1>
+      <Router>
+      {/* here in react-router-dom v6 Switch is replaced with Routes*/}
+        <Routes> 
+          <Route exact path='/' element={<Login />}>
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,109 @@
+import styled from "styled-components";
+
+const Login = (props) => {
+  return (
+    <Container>
+      <Content>
+        <CTA>
+          <CTALogo src="/assets/images/cta-logo-one.svg" alt="" />
+          <SignUp>Get All Here</SignUp>
+          <Description>
+            Disney+ Hotstar is an online video streaming platform owned by Novi
+            Digital Entertainment Private Limited, a wholly owned subsidiary of
+            Star India Private Limited.
+          </Description>
+          <CTALogo src="/assets/images/cta-logo-two.png" alt="" />
+        </CTA>
+        <BgImage />
+      </Content>
+    </Container>
+  );
+};
+
+const Container = styled.section`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 100vh;
+`;
+
+const Content = styled.div`
+  margiin-bottom: 10vw;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-item: center;
+  flex-direction: column;
+  padding: 86px 40px;
+  height: 100%;
+`;
+
+const BgImage = styled.div`
+  height: 100%;
+  background-image: url("/assets/images/login-background.jpg");
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  // z-index will keep the the image behined all the stuff giving it the lowest priority
+  z-index: -1;
+`;
+
+// Call To Action
+const CTA = styled.div`
+  max-width: 650px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-right: auto;
+  margin-left: auto;
+  transition-timing-function: ease-out;
+  transition: opacity 0.2s;
+`;
+
+const CTALogo = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: inline-block;
+  width: 100%;
+  margin-bottom: 20px;
+`;
+
+// SignUp Button
+const SignUp = styled.a`
+  font-weight: bold
+  color: #f9f9f9;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 2.5px;
+  font-size: 20px;
+  font-weight: bolder;
+  padding: 16.5px 0;
+  border: 1px solid transparent;
+  border-radius: 4px;
+
+   &:hover {
+        background-color: #0483ee;
+        cursor: pointer;
+    }
+`;
+
+// Description
+const Description = styled.p`
+  color: hsla(0, 0%, 95.3%, 1);
+  font-size: 11px;
+  margin: 0 0 24px;
+  line-height: 1.5;
+  letter-spacing: 1.5px;
+`;
+
+export default Login;
