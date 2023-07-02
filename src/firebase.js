f21f7776329a43d4c3937bd128874a2b8039b8ea
Omsai <omsainagvekar100@gmail.com>
2023-07-02 20:07:32 +0530

    Seprated firebaseconfig from firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,17 +2,9 @@
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
-import 'firebase/compat/storage'; 
+import 'firebase/compat/storage';
+import firebaseConfig from './firebaseconfig'
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAB22OHWs-jp--BEgDd2zDzEqOSLaKOJew",
-  authDomain: "disneyplushotstarclone-5005c.firebaseapp.com",
-  projectId: "disneyplushotstarclone-5005c",
-  storageBucket: "disneyplushotstarclone-5005c.appspot.com",
-  messagingSenderId: "464550473604",
-  appId: "1:464550473604:web:1c82cde5ab412b6173f7d1"
-};
 
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
